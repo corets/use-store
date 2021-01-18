9f08d0c452fe1d131b2949edead0b571856f5a27
Maxim Kott <maximkott@gmail.com>
2021-01-18 12:26:35 +0100

    Improve build process

--- src/unwrapStore.ts
+++ src/unwrapStore.ts
@@ -1,4 +1,4 @@
-import isFunction from "lodash/isFunction"
+import { isFunction } from "lodash-es"
 import { StoreInitializer } from "./types"
 import { createStore, ObservableStore, Store } from "@corets/store"
 
